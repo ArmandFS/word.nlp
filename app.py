928da51d79102385a97ed79df9c42541d4fe5aeb
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2024-05-29 00:03:47 +0700

    initial app for streamlit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import textdistance  # Not used in this version, but can be explored for further enhancements
+import re
+from collections import Counter
+import nltk
